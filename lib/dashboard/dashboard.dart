1bb5c740474b7db06b30a02bcbbd55397de03f0d
brahim tuijine <brahimtuijine220@gmail.com>
2023-05-03 18:35:11 +0100

    fix login

--- lib/dashboard/dashboard.dart
+++ lib/dashboard/dashboard.dart
@@ -10,15 +10,15 @@ class Dashboard extends HookWidget {
 
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.w600);
-  static const List<Widget> _widgetOptions = <Widget>[
+  static final List<Widget> _widgetOptions = <Widget>[
     HomePage(),
-    Center(
+    const Center(
       child: Text(
         'Camera',
         style: optionStyle,
       ),
     ),
-    AddUser()
+    const AddUser()
   ];
 
   @override
--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:security_camera_project/auth/login_page.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 
 class HomePage extends HookWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
+
+  final List<_SalesData> data = [
+    _SalesData('Jan', 35),
+    _SalesData('Feb', 28),
+    _SalesData('Mar', 34),
+    _SalesData('Apr', 32),
+    _SalesData('May', 40)
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +33,52 @@ class HomePage extends HookWidget {
           ),
         ],
       ),
-      body: Container(),
+      body: Column(children: [
+        //Initialize the chart widget
+        SfCartesianChart(
+            primaryXAxis: CategoryAxis(),
+            // Chart title
+            title: ChartTitle(text: 'Analysis'),
+            // Enable legend
+            legend: Legend(isVisible: true),
+            // Enable tooltip
+            tooltipBehavior: TooltipBehavior(enable: true),
+            series: <ChartSeries<_SalesData, String>>[
+              LineSeries<_SalesData, String>(
+                  dataSource: data,
+                  xValueMapper: (_SalesData sales, _) => sales.year,
+                  yValueMapper: (_SalesData sales, _) => sales.sales,
+                  name: 'Sales',
+                  // Enable data label
+                  dataLabelSettings: const DataLabelSettings(isVisible: true))
+            ]),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            //Initialize the spark charts widget
+            child: SfSparkLineChart.custom(
+              //Enable the trackball
+              trackball: const SparkChartTrackball(
+                  activationMode: SparkChartActivationMode.tap),
+              //Enable marker
+              marker: const SparkChartMarker(
+                  displayMode: SparkChartMarkerDisplayMode.all),
+              //Enable data label
+              labelDisplayMode: SparkChartLabelDisplayMode.all,
+              xValueMapper: (int index) => data[index].year,
+              yValueMapper: (int index) => data[index].sales,
+              dataCount: 5,
+            ),
+          ),
+        )
+      ]),
     );
   }
+}
+
+class _SalesData {
+  _SalesData(this.year, this.sales);
+
+  final String year;
+  final double sales;
 }
