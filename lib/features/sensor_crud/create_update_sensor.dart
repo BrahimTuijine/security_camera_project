f66f1c5dd1560aae78f314c333145413f9354939
brahim tuijine <brahim.tuijne@slnee.com>
2023-06-02 14:10:56 +0100

    create update sensor done

--- lib/features/sensor_crud/create_update_sensor.dart
+++ lib/features/sensor_crud/create_update_sensor.dart
@@ -2,25 +2,26 @@ import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:security_camera_project/core/Model/userModel.dart';
-import 'package:security_camera_project/core/db/user.dart';
+import 'package:security_camera_project/core/Model/sensors_model.dart';
+import 'package:security_camera_project/core/db/sensors.dart';
 
 import 'package:security_camera_project/core/extension/extensions.dart';
 
-class CreateUpdateUser extends HookWidget {
-  final UserModel? userToUpdate;
+class CreateUpdateSensor extends HookWidget {
+  final SensorModel? sensorToUpdate;
 
-  CreateUpdateUser({
+  CreateUpdateSensor({
     super.key,
-    this.userToUpdate,
+    this.sensorToUpdate,
   });
 
   final formKey = GlobalKey<FormState>();
 
-  final Map<String, String> userData = {
-    'name': '',
-    'email': '',
-    'password': '',
+  final Map<String, String> sensorData = {
+    "sensorType": "",
+    "refrences": "",
+    "measuringRange": "",
+    "sustainability": "",
   };
 
   @override
@@ -61,7 +62,7 @@ class CreateUpdateUser extends HookWidget {
                     ),
                   ),
                   Text(
-                    userToUpdate == null ? 'Create user' : 'Update user',
+                    sensorToUpdate == null ? 'Create sensor' : 'Update sensor',
                     style: TextStyle(
                         color: Colors.blue,
                         fontSize: 30.sp,
@@ -69,14 +70,15 @@ class CreateUpdateUser extends HookWidget {
                   ),
                   20.h.bh,
                   TextFormField(
-                    initialValue:
-                        userToUpdate == null ? '' : userToUpdate!.name,
+                    initialValue: sensorToUpdate == null
+                        ? ''
+                        : sensorToUpdate!.sensorType,
                     decoration: InputDecoration(
                       prefixIcon: const Icon(
                         Icons.supervisor_account_sharp,
                         color: Colors.blue,
                       ),
-                      hintText: 'Nom',
+                      hintText: 'Type de capteur',
                       hintStyle: const TextStyle(color: Colors.white),
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(30.r),
@@ -90,20 +92,20 @@ class CreateUpdateUser extends HookWidget {
                       }
                       return null;
                     },
-                    onSaved: (nom) {
-                      userData['name'] = nom!;
+                    onSaved: (sensorType) {
+                      sensorData['sensorType'] = sensorType!;
                     },
                   ),
                   15.h.bh,
                   TextFormField(
                     initialValue:
-                        userToUpdate == null ? '' : userToUpdate!.email,
+                        sensorToUpdate == null ? '' : sensorToUpdate!.refrences,
                     decoration: InputDecoration(
                       prefixIcon: const Icon(
                         Icons.email,
                         color: Colors.blue,
                       ),
-                      hintText: 'E-mail',
+                      hintText: 'Reference',
                       hintStyle: const TextStyle(color: Colors.white),
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(30.r),
@@ -117,20 +119,21 @@ class CreateUpdateUser extends HookWidget {
                       }
                       return null;
                     },
-                    onSaved: (email) {
-                      userData['email'] = email!;
+                    onSaved: (refrences) {
+                      sensorData['refrences'] = refrences!;
                     },
                   ),
                   15.h.bh,
                   TextFormField(
-                    initialValue:
-                        userToUpdate == null ? '' : userToUpdate!.password,
+                    initialValue: sensorToUpdate == null
+                        ? ''
+                        : sensorToUpdate!.measuringRange,
                     decoration: InputDecoration(
                       prefixIcon: const Icon(
                         Icons.lock_outline,
                         color: Colors.blue,
                       ),
-                      hintText: 'Mot de passe',
+                      hintText: 'Plage de mesure',
                       hintStyle: const TextStyle(color: Colors.white),
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(30.r),
@@ -144,8 +147,36 @@ class CreateUpdateUser extends HookWidget {
                       }
                       return null;
                     },
-                    onSaved: (passowrd) {
-                      userData['password'] = passowrd!;
+                    onSaved: (measuringRange) {
+                      sensorData['measuringRange'] = measuringRange!;
+                    },
+                  ),
+                  15.h.bh,
+                  TextFormField(
+                    initialValue: sensorToUpdate == null
+                        ? ''
+                        : sensorToUpdate!.sustainability,
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(
+                        Icons.lock_outline,
+                        color: Colors.blue,
+                      ),
+                      hintText: 'durabilité',
+                      hintStyle: const TextStyle(color: Colors.white),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30.r),
+                          borderSide: BorderSide.none),
+                      fillColor: Colors.blue.withOpacity(.3),
+                      filled: true,
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'ne doit pas être vide';
+                      }
+                      return null;
+                    },
+                    onSaved: (sustainability) {
+                      sensorData['sustainability'] = sustainability!;
                     },
                   ),
                   15.h.bh,
@@ -159,11 +190,12 @@ class CreateUpdateUser extends HookWidget {
                         onPressed: () async {
                           if (formKey.currentState!.validate()) {
                             formKey.currentState!.save();
-                            if (userToUpdate == null) {
-                              UserCRUD().addUser(userData);
+                            if (sensorToUpdate == null) {
+                              SensorsCRUD().addSensor(sensorData);
                             } else {
-                              UserCRUD().updateUser(
-                                  docId: userToUpdate!.id!, newUser: userData);
+                              SensorsCRUD().updateSensor(
+                                  docId: sensorToUpdate!.id!,
+                                  sensor: sensorData);
                             }
                             AwesomeDialog(
                               context: context,
